45b445e2e421b94fe5a2cfa3ff069ca49ed7e384
Dominic DeMark <ddemark@demark.com>
2019-07-21 15:11:00 -0700

    removed service-worker to simplify example

--- ux/src/index.tsx
+++ ux/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
@@ -16,7 +15,3 @@ ReactDOM.render(
     <App />
   </ApolloProvider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
